cce0010d08d90f0b89b83a8e9565f0f8cf4a802d
Kartik Thakore <kthakore@alumni.uwo.ca>
2022-05-16 11:43:28 -0700

    Can query

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,52 +1,82 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
-use arrow::datatypes::{DataType, Field, Schema};
 use arrow::csv::reader::infer_schema_from_files;
-use arrow::record_batch::RecordBatch;
-use arrow::util::pretty::print_batches;
+use arrow::datatypes::{Schema};
+// use arrow::record_batch::RecordBatch;
+// use arrow::util::pretty::print_batches;
 use duckdb::{params, Connection, Result};
+// use serde::*;
+// use serde_json;
+use std::path::Path;
 use std::sync::Mutex;
-
 struct DefragStudioState {
-  pub conn: Mutex<Connection>
+    pub conn: Mutex<Connection>,
 }
 
 fn main() {
-  let conn = Connection::open_in_memory().unwrap();
-  let state = DefragStudioState { conn: Mutex::new(conn) };
-  tauri::Builder::default()
-    .manage(state)
-    .invoke_handler(tauri::generate_handler![load_csv, run_sql])
-    .run(tauri::generate_context!())
-
-    .expect("error while running tauri application");
+    let conn = Connection::open_in_memory().unwrap();
+    let state = DefragStudioState {
+        conn: Mutex::new(conn),
+    };
+    tauri::Builder::default()
+        .manage(state)
+        .invoke_handler(tauri::generate_handler![load_csv, run_sql])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
 
 #[tauri::command]
 fn load_csv(invoke_message: String, state: tauri::State<DefragStudioState>) {
-  let invoke_message: Vec<String> = vec![invoke_message];
-  let schema: Schema = infer_schema_from_files(&invoke_message[..], 0, Some(10), true).unwrap();
-  let conn = state.conn.lock().unwrap();
-  match conn.execute("CREATE TABLE foo ( foo boolean );", params![]) {
-    Ok(_) => {
-      conn.execute("insert into foo values (true);", params![]).unwrap();
-    },
-    Err(e) => {
-      conn.execute("insert into foo values (false);", params![]).unwrap();
+    let invoke_message: Vec<String> = vec![invoke_message];
+    let _table_name: &str = Path::new(&invoke_message[0])
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap();
+    let schema: Schema = infer_schema_from_files(&invoke_message[..], 1, Some(10), true).unwrap();
+    let conn = state.conn.lock().unwrap();
+    match conn.execute("CREATE TABLE foo ( foo boolean );", params![]) {
+        Ok(_) => {
+            for _i in 1..1000 {
+                conn.execute("insert into foo values (true);", params![])
+                    .unwrap();
+            }
+        }
+        Err(_e) => {
+            // for _ in 1..1000 {
+            //     conn.execute("insert into foo values (false);", params![])
+            //         .unwrap();
+            // }
+        }
     }
-  }
-  
-  println!("I was invoked from JS, with this message: {:?}", schema);
+
+    println!("I was invoked from JS, with this message: {:?}", schema);
+}
+
+#[derive(serde::Serialize, Debug, serde::Deserialize)]
+struct DataResponse {
+    records: Vec<bool>,
 }
 
 #[tauri::command]
-fn run_sql(sql: String, state: tauri::State<DefragStudioState>) {
-  let conn = state.conn.lock().unwrap();
-  let mut stmt =  conn.prepare("select * from foo").unwrap();
-  let rbs: Vec<RecordBatch> = stmt.query_arrow([]).unwrap().collect();
-  print_batches(&rbs);
-  println!("I am running {}: {}", sql, rbs.len());
+fn run_sql(sql: String, state: tauri::State<DefragStudioState>) -> Result<DataResponse, String> {
+    let conn = state
+        .conn
+        .lock()
+        .map_err(|_e| String::from("Could not lock connection"))?;
+    let mut stmt = conn
+        .prepare(&sql[..])
+        .map_err(|_e| format!("Could not prepare statement: {}", _e.to_string()))?;
+    let rbs = stmt.query_map([], |row| {
+        let foo: bool = row.get(0)?;
+        Ok(foo)
+    }).map_err(|_e| String::from("Could not query"))?;
+    let rbs: Vec<bool> = rbs.map(|m| m.unwrap()).collect();
+    println!("I am running {}: {:?}", sql, &rbs.len());
+    let records = DataResponse { records: rbs };
+
+    Ok(records)
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import { invoke } from '@tauri-apps/api/tauri'
 import { listen } from '@tauri-apps/api/event'
 
-listen('tauri://file-drop', event => {
-  console.log(event)
-  let files = (event.payload as string[]);
-  if (files.length > 0) { 
-    invoke('load_csv', { invokeMessage: files[0] })
-    invoke('run_sql', { sql: "select * from foo" })
-  }
-})
+import MonacoEditor from 'react-monaco-editor';
+
+type FileDropEvent = {
+  payload: string[]
+}
 
 function App() {
+  const [data, setData] = React.useState<boolean[]>([]);
+  const [tables, setTables] = React.useState<Record<string, boolean>>({});
+  const [sql, setSql] = React.useState<string>("");
+  const [queryError, setQueryError] = React.useState<string | undefined>(undefined);
+  const handleDrop = React.useCallback(async (event: FileDropEvent) => {
+    console.log(event)
+    let files = (event.payload as string[]);
+
+    if (files.length > 0) {
+      
+      await invoke('load_csv', { invokeMessage: files[0] })
+      setTables({"ok":  true})
+    }
+  }, [])
+
+
+  React.useEffect( () => {
+    setData([])
+    setQueryError(undefined)
+    invoke('run_sql', { sql }).then((result) => {
+      let result_typed = result as {records: boolean[]};
+      setData(result_typed.records)
+    }).catch(e => {
+      console.log(e);
+      setQueryError(e)
+    })
+  }, [sql])
+  React.useEffect(() => {
+    console.log("registered", handleDrop)
+    listen('tauri://file-drop', handleDrop)
+  }, []);
+
+  const options = {
+    selectOnLineNumbers: true
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <MonacoEditor
+        width="100%"
+        height="20vh"
+        language="sql"
+        theme="vs-dark"
+        value={sql}
+        options={options}
+        onChange={(v) => setSql(v)}
+      
+      />
+
+      <div style={{color: "red"}}>{queryError}</div>
+        { data.length > 0 &&<table>
+          <thead><tr><td>Foo</td></tr></thead>
+          <tbody>
+          {
+            data.map((item, index) => {
+              return <tr key={index}><td>{item ? "HI" : "BOO"}</td></tr>
+            })
+          }
+          </tbody>
+        </table>}
     </div>
   );
 }
