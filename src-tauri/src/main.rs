247d3d4f9499420e1cc9e758e33ca05c26e8a119
Kartik Thakore <kthakore@alumni.uwo.ca>
2022-05-17 15:35:43 -0700

    More fixes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,22 +7,29 @@ use change_case::snake_case;
 use tracing;
 use tracing_subscriber::fmt::format::FmtSpan;
 
-use std::fs::File;
 use std::path::Path;
-use std::sync::Mutex;
 
-use arrow::datatypes::Schema;
 use arrow::json;
-use arrow::record_batch::RecordBatch;
+use serde_json;
+
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Mutex,
+};
 
 // use arrow::util::pretty::print_batches;
 use duckdb::{params, Connection, Result};
 // use serde::*;
-use serde_json;
+
+#[derive(Debug)]
+struct Running(AtomicBool);
+
+#[derive(Debug)]
+struct Cancelled(AtomicBool);
 
 struct DefragStudioState {
     pub conn: Mutex<Connection>,
-    pub tables: Vec<Schema>,
+    pub query: Option<String>,
 }
 
 fn main() {
@@ -33,11 +40,13 @@ fn main() {
     let conn = Connection::open_in_memory().unwrap();
     let state = DefragStudioState {
         conn: Mutex::new(conn),
-        tables: Vec::new(),
+        query: None,
     };
     tracing::info!("Initializing Defrag Studio");
     tauri::Builder::default()
         .manage(state)
+        .manage(Running(AtomicBool::new(false)))
+        .manage(Cancelled(AtomicBool::new(false)))
         .invoke_handler(tauri::generate_handler![load_csv, run_sql])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
@@ -87,13 +96,27 @@ struct DataResponse {
 }
 
 #[tauri::command]
+#[tracing::instrument(skip(cancel), err)]
+async fn cancel(cancel: bool, cancelled: tauri::State<'_, Cancelled>) -> Result<(), String> {
+    cancelled.0.store(cancel, Ordering::Relaxed);
+    Ok(())
+}
+
+#[tauri::command]
 #[tracing::instrument(skip(state, window), err)]
 async fn run_sql(
     sql: String,
     state: tauri::State<'_, DefragStudioState>,
+    cancel: tauri::State<'_, Cancelled>,
+    running:  tauri::State<'_, Running>,
     window: tauri::Window,
 ) -> Result<(), String> {
-    tracing::info!("Running csv");
+    let is_running = running.0.load(Ordering::Relaxed);
+    if is_running {
+      cancel.0.store(true, Ordering::Relaxed);
+      running.0.store(false, Ordering::Relaxed);
+      tracing::info!("Running csv");
+    }
     let conn = state
         .conn
         .lock()
@@ -108,16 +131,25 @@ async fn run_sql(
     // let rbs: Vec<bool> = rbs.map(|m| m.unwrap()).collect();
     tracing::info!("Loading arrow");
     let batches = stmt.query_arrow(params![]).map_err(|e| e.to_string())?;
-
+    cancel.0.store(false, Ordering::Relaxed);
+    running.0.store(true, Ordering::Relaxed);
     for batch in batches {
-        let _span = tracing::info_span!("batch", size=batch.num_rows()).entered();
+        let cancelled: bool = cancel.0.load(Ordering::Relaxed);
+        tracing::info!("Cancelled: {}", cancelled);
+        if !cancelled {
+            let _span = tracing::info_span!("batch", size = batch.num_rows()).entered();
 
-        let json_rows: Vec<serde_json::Map<String, serde_json::Value>> =
-            json::writer::record_batches_to_json_rows(&vec![batch][..]).unwrap();
-        window
-            .emit("load_arrow_row_batch", json_rows)
-            .map_err(|e| e.to_string())?
+            let json_rows: Vec<serde_json::Map<String, serde_json::Value>> =
+                json::writer::record_batches_to_json_rows(&vec![batch][..]).unwrap();
+            window
+                .emit("load_arrow_row_batch", json_rows)
+                .map_err(|e| e.to_string())?
+        } else {
+          cancel.0.store(false, Ordering::Relaxed);
+          break;
+        }
     }
+    running.0.store(false, Ordering::Relaxed);
     // tracing::info!("Serializing arrow");
     //
     // tracing::info!("Finished Serializing {}: {:?}", sql, &rbs.len());
