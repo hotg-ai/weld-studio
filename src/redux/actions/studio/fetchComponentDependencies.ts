da2a0168bf111a0b4792d15afc5ab28fcafb215f
Anugrah Adams <anugrah.adams@gmail.com>
2022-05-31 14:52:11 +0530

    Compatibility fixes

--- src/redux/actions/studio/fetchComponentDependencies.ts
+++ src/redux/actions/studio/fetchComponentDependencies.ts
@@ -1,48 +1,39 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { ThunkApiConfig } from "../project";
-import { fileAlreadyExists } from "./uploadFile";
 import { uploadModel } from "./uploadModel";
 
 type FetchComponentDependencies = {
-    componentID: string;
-    token: string;
+  componentID: string;
+  token: string;
 };
 
 export const fetchComponentDependencies = createAsyncThunk<
-    void,
-    FetchComponentDependencies,
-    ThunkApiConfig
+  void,
+  FetchComponentDependencies,
+  ThunkApiConfig
 >("builder/fetchComponentDependencies", async ({ componentID, token }, api) => {
-    const {
-        builder: { components, forgeBuilder, project }
-    } = api.getState();
+  const {
+    builder: { components, project },
+  } = api.getState();
 
-    if (!(componentID in components)) {
-        // if we don't already know about this component, there's nothing to worry
-        // about
-        return;
-    }
+  if (!(componentID in components)) {
+    // if we don't already know about this component, there's nothing to worry
+    // about
+    return;
+  }
 
-    const component = components[componentID];
+  const component = components[componentID];
 
-    if (
-        !forgeBuilder ||
-        project.state !== "loaded" ||
-        component.type !== "model"
-    ) {
-        return;
-    }
+  if (project.state !== "loaded" || component.type !== "model") {
+    return;
+  }
 
-    const { identifier, displayName, source, downloadURL } = component;
-    if (source !== "builtin" || !downloadURL) {
-        return;
-    }
+  const { identifier, displayName, source, downloadURL } = component;
+  if (source !== "builtin" || !downloadURL) {
+    return;
+  }
 
-    if (await fileAlreadyExists(forgeBuilder, project.info.id, identifier)) {
-        return;
-    }
-
-    const response = await fetch(downloadURL);
-    const model = await response.arrayBuffer();
-    api.dispatch(uploadModel({ displayName, model, path: identifier, token }));
+  const response = await fetch(downloadURL);
+  const model = await response.arrayBuffer();
+  api.dispatch(uploadModel({ displayName, model, path: identifier, token }));
 });
