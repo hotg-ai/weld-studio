4c270d4abdcced6fd358cd9b2bab44df5d43af87
Anugrah Adams <anugrah.adams@gmail.com>
2022-06-03 18:08:47 +0530

    Handling no data on click

--- src/components/Dataset/components/table/index.tsx
+++ src/components/Dataset/components/table/index.tsx
@@ -2,23 +2,27 @@ import React from "react";
 import "./table.css";
 
 const Table = ({ data }: { data: any[] }) => {
+  if (!data || data.length === 0) return;
   let header = Object.keys(data[0]);
   return (
     <table>
       <thead>
         <tr>
-          {header.map((h) => <th key={h}>{h}</th>)}
+          {header.map((h) => (
+            <th key={h}>{h}</th>
+          ))}
         </tr>
       </thead>
       <tbody>
         {data.map((d, idx) => {
           return (
             <tr key={idx}>
-              {header.map((h) => <td key={h}>{d[h]}</td>)}
+              {header.map((h) => (
+                <td key={h}>{d[h]}</td>
+              ))}
             </tr>
-          )
+          );
         })}
-
       </tbody>
     </table>
   );
