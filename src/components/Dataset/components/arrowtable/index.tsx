4127f5eb6f239275cf8641088f5829745bdce556
Anugrah Adams <anugrah.adams@gmail.com>
2022-06-23 05:01:02 +0530

    Implementing React Table with Virtual Rows

--- src/components/Dataset/components/arrowtable/index.tsx
+++ src/components/Dataset/components/arrowtable/index.tsx
@@ -1,47 +1,113 @@
 import "./table.css";
+import React from "react";
+import { useTable, useBlockLayout } from "react-table";
+import { FixedSizeList } from "react-window";
+import scrollbarWidth from "./scrollbarwidth";
 
-const ArrowTable = ({ data }: { data: any[] }) => {
-  let header = [];
-  if (data !== undefined && data.length > 0) {
-    if (data[0]) header = Object.keys(data[0].toJSON());
+function VTable({ columns, data }: { columns: any[]; data: any[] }) {
+  // Use the state and functions returned from useTable to build your UI
 
-    return (
-      <table>
-        <thead>
-          <tr>
-            {header.map((h) => (
-              <th key={h}>
-                {h}{" "}
-                {/* {h === header[header.length - 1] && (
-                  <button>
-                    <img src={downloadIcon} alt="" /> Download
-                  </button>
-                )} */}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((row, idx) => {
-            const d = row.toJSON();
+  const defaultColumn = React.useMemo(
+    () => ({
+      width: 120,
+    }),
+    []
+  );
+
+  const scrollBarSize = React.useMemo(() => scrollbarWidth(), []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    totalColumnsWidth,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn,
+    },
+    useBlockLayout
+  );
+
+  const RenderRow = React.useCallback(
+    ({
+      index,
+      style,
+    }: {
+      index: number;
+      style: React.CSSProperties | undefined;
+    }) => {
+      const row = rows[index];
+      prepareRow(row);
+      return (
+        <div
+          {...row.getRowProps({
+            style,
+          })}
+          className="tr"
+        >
+          {row.cells.map((cell) => {
             return (
-              <tr key={idx}>
-                {header.map((h) =>
-                  d[h] !== undefined ? (
-                    <td key={h}>{d[h]}</td>
-                  ) : (
-                    <td key={h}>
-                      <i>null</i>
-                    </td>
-                  )
-                )}
-              </tr>
+              <div {...cell.getCellProps()} className="td">
+                {cell.render("Cell")}
+              </div>
             );
           })}
-        </tbody>
-      </table>
-    );
-  } else return null;
+        </div>
+      );
+    },
+    [prepareRow, rows]
+  );
+
+  // Render the UI for your table
+  return (
+    <div {...getTableProps()} className="table">
+      <div>
+        {headerGroups.map((headerGroup) => (
+          <div {...headerGroup.getHeaderGroupProps()} className="tr">
+            {headerGroup.headers.map((column) => (
+              <div {...column.getHeaderProps()} className="th">
+                {column.render("Header")}
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+
+      <div {...getTableBodyProps()}>
+        <FixedSizeList
+          height={400}
+          itemCount={rows.length}
+          itemSize={35}
+          width={totalColumnsWidth + scrollBarSize}
+        >
+          {RenderRow}
+        </FixedSizeList>
+      </div>
+    </div>
+  );
+}
+
+declare type Column = { Header: string; accessor: string };
+
+const computeColumns = (header: string[]): Column[] => {
+  let columns: Column[] = [];
+  header.forEach((head) => {
+    columns.push({
+      Header: head,
+      accessor: head,
+    });
+  });
+  return columns;
+};
+
+const ArrowTable = ({ data }: { data: any[] }) => {
+  const header = Object.keys(data[0].toJSON());
+  const columns = React.useMemo(() => computeColumns(header), [data]);
+  return <VTable columns={columns} data={data} />;
 };
 
 export default ArrowTable;
